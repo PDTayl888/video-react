913e73e63eca52702aebba6c4346d581e35d0409
pdtayl888 <patrickdtaylor1980@gmail.com>
2020-02-13 12:20:45 -0500

    typos

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import React from 'react';
 import './App.css';
 import Movies from './components/movies';
 
-const App = () => (params) {
+const App = params => {
   return (
     <main className='container'>
       <Movies />
     </main>
   );
-}
+};
 
 export default App;
--- src/components/common/like.jsx
+++ src/components/common/like.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Like = ({ onClick, liked }) => {
   let classes = 'fa fa-heart';
-  if (!liked) classes += '-o';
+  if (liked) classes += '-o';
   return (
     <i
       onClick={onClick}
