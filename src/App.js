6a3ee3acc7823e7a69e569b9395f88ca072904c8
pdtayl888 <patrickdtaylor1980@gmail.com>
2020-02-16 23:13:15 -0500

    added routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import './App.css';
+import { Route } from 'react-router-dom';
 import Movies from './components/movies';
+import Customers from './components/customers';
+import Rentals from './components/rentals';
+import NotFound from './components/notFound';
+import './App.css';
 
 const App = params => {
   return (
     <main className='container'>
-      <Movies />
+      <Route path='/movies' component={Movies}></Route>
+      <Route path='/customers' component={Customers}></Route>
+      <Route path='/rentals' component={Rentals}></Route>
+      <Route path='/not-found' component={NotFound}></Route>
     </main>
   );
 };
