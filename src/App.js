ac5fa6216dfa37d21cac8a1e7b1e61c030560cab
pdtayl888 <patrickdtaylor1980@gmail.com>
2020-02-18 17:12:20 -0500

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,20 +1,30 @@
-import React from 'react';
-import { Route } from 'react-router-dom';
+import React, { Component } from 'react';
+import { Route, Redirect, Switch } from 'react-router-dom';
 import Movies from './components/movies';
 import Customers from './components/customers';
+import NavBar from './components/navBar';
 import Rentals from './components/rentals';
 import NotFound from './components/notFound';
 import './App.css';
 
-const App = params => {
-  return (
-    <main className='container'>
-      <Route path='/movies' component={Movies}></Route>
-      <Route path='/customers' component={Customers}></Route>
-      <Route path='/rentals' component={Rentals}></Route>
-      <Route path='/not-found' component={NotFound}></Route>
-    </main>
-  );
-};
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <NavBar />
+        <main className='container'>
+          <Switch>
+            <Route path='/movies' component={Movies}></Route>
+            <Route path='/customers' component={Customers}></Route>
+            <Route path='/rentals' component={Rentals}></Route>
+            <Route path='/not-found' component={NotFound}></Route>
+            <Redirect from='/' exact to='/movies' />
+            <Redirect to='/not-found' />
+          </Switch>
+        </main>
+      </React.Fragment>
+    );
+  }
+}
 
 export default App;
